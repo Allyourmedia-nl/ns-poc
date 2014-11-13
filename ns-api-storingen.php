48b50561e4bbd0042812cabc76d3261f24c969b4
aym-ad <ad@allyourmedia.nl>
2014-11-13 15:27:51 +0100

    Initial commit
    De oorspronkelijke staat van dit project

---
+++ ns-api-storingen.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+require_once('includes/ns.class.php');
+$ns = new NS( new curl( new user( 'HJ@allyourmedia.nl', 'UQ3d07vfmfLr9Z0Y7koCtrLWvwLQuBgBS_V01rK0ONQ0HPVN6EWNBw' ) ) );
+//vertrektijden
+$vertrektijden = $ns->getDepartures('Arnhem');
+
+//prijzen
+//$prijzen = $ns->getPrices('Delfzijl', 'Appingedam', '',date("dmY"));
+
+//reisadvies
+$advies = $ns->getAdvise('Delfzijl', 'Groningen', 'Appingedam', date("dmY"));
+
+//storingen
+$storingen = $ns->getDisturbances('Arnhem');
+//echo json_encode($stations);
+//echo json_encode($vertrektijden);
+echo json_encode($storingen);
+?>
